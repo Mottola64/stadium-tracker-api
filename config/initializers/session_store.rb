2d55033108d72b71c4cb81d794aad9f86d600bf4
Michael Mottola <mottola64@gmail.com>
2020-06-08 19:15:05 -0400

    fixed gemfile

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_your-app-name', domain: 'your-frontend-domain'
+    Rails.application.config.session_store :cookie_store, key: '_stadium-tracker', domain: 'your-frontend-domain'
   else
-    Rails.application.config.session_store :cookie_store, key: '_your-app-name' 
+    Rails.application.config.session_store :cookie_store, key: '_stadium-tracker' 
   end
