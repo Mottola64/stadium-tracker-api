28666aeb37f44e0fded761394501d7ce217ec707
Michael Mottola <mottola64@gmail.com>
2020-06-22 15:23:50 -0400

    add team and stadium models

---
+++ db/migrate/20200622192153_create_stadia.rb
@@ -0,0 +1,11 @@
+class CreateStadia < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stadia do |t|
+      t.string :name
+      t.string :city
+      t.string :team
+
+      t.timestamps
+    end
+  end
+end
